f5e469e3a55c04a6a467eba31afc325451358106
Ashim Dahal <codeashim@gmail.com>
2024-02-12 18:47:44 -0600

    feat: added svc, decision tree, and logistic regression to random forrest

--- model_synergy_phase/post_mortem_analysis.py
+++ model_synergy_phase/post_mortem_analysis.py
@@ -1,5 +1,8 @@
 import joblib
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 import pandas as pd
@@ -18,7 +21,19 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
-clf = RandomForestClassifier(max_depth=5, random_state=42)
-clf.fit(X_train, y_train)
+rf_clf = RandomForestClassifier(max_depth=5, random_state=42)
+rf_clf.fit(X_train, y_train)
+joblib.dump(rf_clf, "randomforrest_classifier.joblib")
 
-joblib.dump(clf, "randomforrest_classifier.joblib")
+
+dt_clf = DecisionTreeClassifier(random_state=42, max_depth=5)
+dt_clf.fit(X, y)
+joblib.dump(dt_clf, "decisiontree_classifier.joblib")
+
+svc_clf = LinearSVC(dual="auto", tol=1e-05, random_state=42)
+svc_clf.fit(X, y)
+joblib.dump(svc_clf, "supportvector_classifier.joblib")
+
+lr_clf = LogisticRegression(random_state=42)
+lr_clf.fit(X, y)
+joblib.dump(lr_clf, "LogisticRegression_classifier.joblib")
